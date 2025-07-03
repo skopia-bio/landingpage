21f0539f87431da44831262ba6c6709c88225733
rolo5555 <rolo.loga@gmail.com>
2025-07-03 00:21:53 -0300

    Update base path for root domain deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ladingpage/',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(import.meta.dirname, "client", "src"),
